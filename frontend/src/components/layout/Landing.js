550277c7ea0897d17478780f7f7ea4594edfeba4
Shady7781 <troudishedy6@gmail.com>
2021-08-19 11:38:41 +0200

    improve design and add cards

--- frontend/src/components/layout/Landing.js
+++ frontend/src/components/layout/Landing.js
@@ -4,19 +4,29 @@ import { Link } from "react-router-dom";
 class Landing extends Component {
   render() {
     return (
-      <div style={{ height: "75vh" }} className="container valign-wrapper">
+      <div className="container">
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+     
         <div className="row">
           <div className="col s12 center-align">
             <h4>
-              <b>Build</b> a login/auth app with the{" "}
+              <b>Learn new skills</b> a login/auth app with the{" "}
               <span style={{ fontFamily: "monospace" }}>MERN</span> stack from
               scratch
             </h4>
             <p className="flow-text grey-text text-darken-1">
-              Create a (minimal) full-stack app with user authentication via
-              passport and JWTs
+              Our mission is to spread education that is easy accessible and everyone can learn.
+
             </p>
             <br />
+
             <div className="col s6">
               <Link
                 to="/register"
@@ -30,6 +40,7 @@ class Landing extends Component {
                 Register
               </Link>
             </div>
+            
             <div className="col s6">
               <Link
                 to="/login"
@@ -44,8 +55,145 @@ class Landing extends Component {
               </Link>
             </div>
           </div>
+          
         </div>
-      </div>
+
+
+<br></br>
+<br></br>
+        <div className="row">
+          
+
+  
+  <div class="card small" style={{
+
+margin: "auto",
+width: "50%",
+padding: "10px"
+  }}>
+    <div class="card-image waves-effect waves-block waves-light">
+      <img class="activator" 
+      
+      style={{width :"70vh",height:"25vh" , padding:"10px"}}
+      src="https://blog.cellenza.com/wp-content/uploads/2015/05/React-JS-1242x640.png"/ >
+    </div>
+    <div class="card-content">
+      <span class="card-title activator grey-text text-darken-4">REACT<i class="material-icons right">more_vert</i></span>
+      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+
+</p>
+    </div>
+    <div class="card-reveal">
+      <span class="card-title grey-text text-darken-4">Mountain Journey<i class="material-icons right">close</i></span>
+      <p>Here is some more information about this product that is only revealed once clicked on.</p>
+    </div>
+  </div>
+
+
+
+        </div>
+        <div className="row">
+          
+
+  
+          <div class="card small" style={{
+        
+        margin: "auto",
+        width: "50%",
+        padding: "10px"
+          }}>
+            <div class="card-image waves-effect waves-block waves-light">
+              <img class="activator" 
+              
+              style={{width :"70vh",height:"20vh" , padding:"10px"}}
+              src="https://blog.cellenza.com/wp-content/uploads/2015/05/React-JS-1242x640.png"/ >
+            </div>
+            <div class="card-content">
+              <span class="card-title activator grey-text text-darken-4">REACT<i class="material-icons right">more_vert</i></span>
+              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+        
+        </p>
+            </div>
+            <div class="card-reveal">
+              <span class="card-title grey-text text-darken-4">Mountain Journey<i class="material-icons right">close</i></span>
+              <p>Here is some more information about this product that is only revealed once clicked on.</p>
+            </div>
+          </div>
+        
+        
+        
+                </div>
+                <div className="row">
+          
+
+  
+          <div class="card small" style={{
+        
+        margin: "auto",
+        width: "50%",
+        padding: "10px"
+          }}>
+            <div class="card-image waves-effect waves-block waves-light">
+              <img class="activator" 
+              
+              style={{width :"70vh",height:"20vh" , padding:"10px"}}
+              src="https://discoverprograming.com/wp-content/uploads/2020/06/Angular-9.png"/ >
+            </div>
+            <div class="card-content">
+              <span class="card-title activator grey-text text-darken-4">REACT<i class="material-icons right">more_vert</i></span>
+              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+        
+        </p>
+            </div>
+            <div class="card-reveal">
+              <span class="card-title grey-text text-darken-4">Mountain Journey<i class="material-icons right">close</i></span>
+              <p>Here is some more information about this product that is only revealed once clicked on.</p>
+            </div>
+          </div>
+        
+        
+        
+                </div>
+                <div className="row">
+          
+
+  
+          <div class="card small" style={{
+        
+        margin: "auto",
+        width: "50%",
+        padding: "10px"
+          }}>
+            <div class="card-image waves-effect waves-block waves-light">
+              <img class="activator" 
+              
+              style={{width :"70vh",height:"20vh" , padding:"10px"}}
+              src="https://www.developpez.net/forums/attachments/p579609d1/a/a/a"/>
+            </div>
+            <div class="card-content">
+              <span class="card-title activator grey-text text-darken-4">REACT<i class="material-icons right">more_vert</i></span>
+              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+        
+        </p>
+            </div>
+            <div class="card-reveal">
+              <span class="card-title grey-text text-darken-4">Mountain Journey<i class="material-icons right">close</i></span>
+              <p>Here is some more information about this product that is only revealed once clicked on.</p>
+            </div>
+          </div>
+        
+        
+        
+                </div>
+
+                
+                
+     </div>
+     
     );
   }
 }
--- frontend/src/components/layout/Navbar.js
+++ frontend/src/components/layout/Navbar.js
@@ -13,8 +13,8 @@ class Navbar extends Component {
               }}
               className="col s5 brand-logo center black-text"
             >
-              <i className="material-icons">code</i>
-              MERN
+              <i className="material-icons">share</i>
+              SHARI
             </Link>
           </div>
         </nav>
